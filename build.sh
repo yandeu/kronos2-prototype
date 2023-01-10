aa677fdd682269571cfd5cb8bb3ce1bc570c37b3
yandeu <20306025+yandeu@users.noreply.github.com>
2023-01-10 15:06:25 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
+
 echo "Building Krono's 2 App:"
 echo ""
 echo "> cleaning www folder..."
 rm -rf www
+mkdir www
 echo "> removing app.zip..."
 rm -rf app.zip
 echo "> build app using webpack..."
-./node_modules/.bin/webpack --config webpack/webpack.prod.mjs
+./node_modules/.bin/webpack --config ./webpack/webpack.prod.mjs
 echo "> copying static files..."
 cp -rf ./static/* ./www
+echo "> list all files..."
+ls
 echo "> zipping all files..."
 zip -r app.zip assets www config.json
 echo "> done!"
